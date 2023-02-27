f76ed44384abd41ec8df1196c50310124b0879b8
EzeSandes <jezesandes@gmail.com>
2023-02-27 15:01:52 -0300

    feat: added note CRUD functions

--- server/controllers/notesController.js
+++ server/controllers/notesController.js
@@ -1,14 +1,84 @@
 const Note = require('../models/noteModel');
 
-exports.getAllNotes = async function (req, res) {
+exports.getAllNotes = async (req, res) => {
   try {
     const notes = await Note.find();
 
     res.status(200).json({
       status: 'success',
-      notes,
+      data: { notes },
     });
   } catch (error) {
-    console.log(error);
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+exports.getNote = async (req, res) => {
+  try {
+    const note = await Note.findById(req.params.id);
+
+    res.status(200).json({
+      status: 'success',
+      data: { note },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+exports.createNote = async (req, res) => {
+  try {
+    const newNote = await Note.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: { newNote },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent!',
+    });
+  }
+};
+
+exports.updateNote = async (req, res) => {
+  try {
+    const updatedNote = await Note.findByIdAndUpdate(req.params.is, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(201).json({
+      status: 'success',
+      data: { updatedNote },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent!',
+    });
+  }
+};
+
+exports.deleteNote = async (req, res) => {
+  try {
+    await Note.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
   }
 };
--- server/routes/noteRoutes.js
+++ server/routes/noteRoutes.js
@@ -5,7 +5,16 @@ const notesController = require('../controllers/notesController');
 
 //////////////
 
-router.route('/').get(notesController.getAllNotes);
+router
+  .route('/')
+  .get(notesController.getAllNotes)
+  .post(notesController.createNote);
+
+router
+  .route('/:id')
+  .get(notesController.getNote)
+  .delete(notesController.deleteNote)
+  .patch(notesController.updateNote);
 
 //////////////
 
